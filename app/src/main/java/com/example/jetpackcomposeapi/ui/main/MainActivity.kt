3426d4b13b5aa979d7c35a5d283777680a7c4232
Gegerout <selimchik06@gmail.com>
2023-08-27 21:47:23 +0300

    ui update

---
+++ app/src/main/java/com/example/jetpackcomposeapi/ui/main/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jetpackcomposeapi.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.jetpackcomposeapi.ui.home.HomeScreen
+import com.example.jetpackcomposeapi.ui.theme.JetpackComposeApiTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeApiTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
